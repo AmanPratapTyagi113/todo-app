30e5e9c1efdc7f25b18dcc26aceec4b7e05cd0c3
AmanPratapTyagi113 <amanprataptyagi113@gmail.com>
2023-03-27 22:12:36 +0530

    Updates

---
+++ precache-manifest.04d162f200497748ed9f847983eb69b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1273f698843ddd8b7f0",
+    "url": "/static/css/main.06a4cee7.chunk.css"
+  },
+  {
+    "revision": "f1273f698843ddd8b7f0",
+    "url": "/static/js/main.f1273f69.chunk.js"
+  },
+  {
+    "revision": "1fe23a47e4773fd9b87f",
+    "url": "/static/js/1.1fe23a47.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "034c2fb4b631888efc2add4596a2aef2",
+    "url": "/index.html"
+  }
+];
